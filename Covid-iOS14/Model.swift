27ee49fabe8f9d10fe700382543384d8d7537ef9
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-06-12 14:24:22 -0400

    init commit

---
+++ Covid-iOS14/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  Covid-iOS14
+//
+//  Created by Matthew Garlington on 6/12/22.
+//
+
+import Foundation
+
+struct CovidInfo: Hashable, Decodable {
+    var submission_date: String
+    var state: String
+    var tot_cases: String
+    var conf_cases: String?
+    var prob_cases: String?
+    var new_case: String?
+    var pnew_case: String?
+    var tot_death: String?
+    var pnew_death: String?
+    var created_at: String?
+    var consent_cases: String?
+    var consent_deaths: String?
+}
---
+++ Covid-iOS14/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  Covid-iOS14
+//
+//  Created by Matthew Garlington on 6/12/22.
+//
+
+import Foundation
+import Combine
+
+class NetworkManager: ObservableObject {
+    @Published var covidInfo: [CovidInfo] = []
+    
+    var cancellables: Set<AnyCancellable> = []
+    
+    func fetch(dateFetched: String) {
+
+        let url = URL(string: "https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=\(dateFetched)")!
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: [CovidInfo].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                if case .failure(let error) = completion {
+                    print("Error! \(error.localizedDescription)")
+                }
+            } receiveValue: { [unowned self] covidInfoReceived in
+                covidInfo = covidInfoReceived
+                print("Success!!!")
+            }
+            .store(in: &cancellables)
+    }
+}
