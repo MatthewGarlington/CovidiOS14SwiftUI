e909c4eef5c9adb1d96dfcdfd99d3dd8692837b2
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-06-12 06:09:23 -0400

    Initial Commit

---
+++ Covid-iOS14/Covid_iOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_iOS14App.swift
+//  Covid-iOS14
+//
+//  Created by Matthew Garlington on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Covid_iOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
